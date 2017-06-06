48801629fa28a4e843ffe7cc9c74af1b6c85b34d
Matt Turner <matturner@gmail.com>
2017-06-06 14:34:35 +0100

    Begin to rearrange frontend as a package rather than a module.

--- fe/build.sh
+++ fe/build.sh
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/sysstats.proto
+python -m grpc_tools.protoc -I../protos --python_out=frontend --grpc_python_out=frontend ../protos/sysstats.proto
--- fe/run.sh
+++ fe/run.sh
@@ -1,2 +1,2 @@
-export FLASK_APP=main.py
+export FLASK_APP=frontend/init.py
 python -m flask run
