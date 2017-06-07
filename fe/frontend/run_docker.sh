efb5ed732916f89dc8932ec4885f761a4d368b90
Matt Turner <matturner@gmail.com>
2017-06-07 19:38:15 +0000

    Expose uWSGI, and on external port

--- fe/frontend/run_docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --name sysstats_frontend --rm -p 32770:32770 --link sysstats_backend:sysstats_backend sysstats_frontend
---
+++ fe/run_docker.sh
@@ -0,0 +1,1 @@
+docker run --name sysstats_frontend --rm -p 3031:3031 --link sysstats_backend:sysstats_backend sysstats_frontend
