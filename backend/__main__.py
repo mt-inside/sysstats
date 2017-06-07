da8000470ea1fa006a676cc9b235d878b2f81849
Matt Turner <matturner@gmail.com>
2017-06-07 20:14:09 +0000

    Move sysstats server class

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,60 +1,9 @@
 import time
-import math
-import os
 from concurrent import futures
-import subprocess
 
 import grpc
-
-import sysstats_pb2
 import sysstats_pb2_grpc
-
-class SysStatsServicer(sysstats_pb2_grpc.SysStatsServicer):
-    def Date(
-        self,
-        request, # dict of the args
-        context # call info like timeout
-    ):
-        return sysstats_pb2.DateT(
-            date = math.trunc(time.time())
-        )
-
-    def Os(self, request, context):
-        u = os.uname()
-        uname = sysstats_pb2.UnameT(nodename=u.nodename, sysname=u.sysname, release=u.release, version=u.version, machine=u.machine)
-
-        kernel = "<unknown /proc/version>"
-        try:
-            ver = open('/proc/version')
-        except IOError:
-            pass # not on Mac for example
-        else:
-            with ver:
-                kernel = ver.read()
-
-
-        return sysstats_pb2.OsT(
-            uname_a = uname,
-            kernel_version = kernel
-        )
-
-    def Ifaces(self, request, context):
-        try:
-            # Hacks to get round the buffering on the pipe (and the fact that netstat doesn't flush it)
-            # Had no luck with:
-            # - reopening the pipe fd with no buffering
-            # - using (g)stdbuf
-            # - didn't try expect's unbuffered (not built by the stanard brew build of expect)
-            sys = os.uname().sysname
-            if sys == 'Darwin':
-                proc = subprocess.Popen(["script", "-qF", "/dev/null", "netstat", "-ibd"], stdout=subprocess.PIPE)
-            elif sys == 'Linux':
-                proc = subprocess.Popen(["script", "-qf", "-c", "netstat -i", "/dev/null"], stdout=subprocess.PIPE)
-            ip = proc.stdout
-
-            return (sysstats_pb2.IfaceT(line=l) for l in iter(ip.readline, b''))
-        except:
-            pass
+from sysstats_server import SysStatsServicer
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
--- frontend/frontend/run.sh
+++ frontend/frontend/run.sh
@@ -1,2 +1,2 @@
-export FLASK_APP=frontend/init.py
+export FLASK_APP=init.py
 python -m flask run
